9e14846fdeb3b6ea1c3d3c978547ba0090676b34
Walt Shands <jshands@ucsc.edu>
2020-03-11 17:46:22 +0000

    remove comments from code

--- snapAnalysis.R
+++ snapAnalysis.R
@@ -449,136 +449,136 @@ plotViz(
 
 
 
-#message(sprintf("Creating cell by gene matrix\n"))
-#genes = read.table(opt$gene_track);
-#genes.gr = GRanges(genes[,1],
-#  IRanges(genes[,2], genes[,3]), name=genes[,4]
-#);
-#
-#message(sprintf("Writing gene ranges to csv\n"))
-#genes_df = as(genes.gr, "data.frame")
-#write.csv(genes_df, file = "GenesRanges.csv", row.names = FALSE)
-#
-#x.sp = createGmatFromMat(
-#  obj=x.sp, 
-#  input.mat="bmat",
-#  genes=genes.gr,
-#  do.par=TRUE,
-#  num.cores=10
-#);
-#
-## smooth the cell-by-gene matrix
-#message(sprintf("Smoothing the cell-by-gene matrix\n"))
-#x.sp = runMagic(
-#  obj=x.sp,
-#  input.mat="gmat",
-#  step.size=3
-#);
-#
-#message(sprintf("Writing the cell by gene data to a Matrix Market format file\n"))
-#cellByGeneData <- x.sp@gmat;
-#cellByGeneMatrix <- as.matrix(cellByGeneData)
-#cellByGene <- as.data.frame(cellByGeneMatrix);
-#
-## Write cell by gene sparse matrix in Matrix Market format not CSV format
-##write.csv(cellByGene, file = "cellByGeneData.csv");
-#writeMM(obj = cellByGeneData, file = "cellByGene.mtx")
-#
-#
-## We don't need the cell by gene summary csv except for perhaps debugging
-##cellByGeneSummary <- summary(cellByGeneData);
-##
-### Rename the summary column names i,j
-###https://stackoverflow.com/questions/7531868/how-to-rename-a-single-column-in-a-data-frame
-##names(cellByGeneSummary)[names(cellByGeneSummary) == 'i'] <- 'BarcodeID'
-##names(cellByGeneSummary)[names(cellByGeneSummary) == 'j'] <- 'GeneID'
-##
-### Update: only including the numeric barcode IDs in most files, since most people won't
-### care about the actual barcodes for most uses, and including these in every single
-### file just has the effect of inflating the file sizes. If people actually do care about
-### the barcode sequence of a cell, they'll still be able to use 'cellBarcodes.csv' to obtain it.
-#### Add barcodes column to cell by bin 
-###cellByGeneSummary$Barcode <- BarcodeLookupTable[match(cellByGeneSummary$BarcodeID, BarcodeLookupTable$BarcodeID), "Barcode"]
-##
-### Get the gene names from the sparse matrix
-##summaryGeneNames <- colnames(cellByGene)[cellByGeneSummary$GeneID]
-##cellByGeneSummary$Gene <- summaryGeneNames
-##
-##message(sprintf("Writing the cell by gene summary to a csv\n"))
-##write.csv(cellByGeneSummary, file = "cellByGene_summary.csv", row.names = FALSE);
-#
-#message(sprintf("Writing the gene id to gene name lookup table to a csv\n"))
-## Create a lookup table for gene ID to gene name string
-##http://best-answer.net/easy-way-to-perform-a-lookup-in-r/
-#geneNames <- colnames(cellByGene)
-#geneIDs <- c(1:length(geneNames))
-#geneLookupTable <- data.frame("GeneID" = geneIDs, "Gene" = geneNames);
-#write.csv(geneLookupTable, file = "cellGenes.csv", row.names = FALSE);
-#
+message(sprintf("Creating cell by gene matrix\n"))
+genes = read.table(opt$gene_track);
+genes.gr = GRanges(genes[,1],
+  IRanges(genes[,2], genes[,3]), name=genes[,4]
+);
 
-## Step 11. Identify peaks
-## Next we aggregate cells from the each cluster to create an ensemble track for
-## peak calling and visualization. This step will generate a .narrowPeak file that
-## contains the identified peak and .bedGraph file for visualization. To obtain
-## the most robust result, we don't recommend to perform this step for clusters
-## with cell number less than 100. In the below example, SnapATAC creates
-## atac_v1_adult_brain_fresh_5k.1_peaks.narrowPeak and 
-## atac_v1_adult_brain_fresh_5k.1_treat_pileup.bdg. bdg file can be compressed
-## to bigWig file using bedGraphToBigWig for IGV or Genome Browser visulization.
-#message(sprintf("Identifying peaks\n"))
+message(sprintf("Writing gene ranges to csv\n"))
+genes_df = as(genes.gr, "data.frame")
+write.csv(genes_df, file = "GenesRanges.csv", row.names = FALSE)
+
+x.sp = createGmatFromMat(
+  obj=x.sp, 
+  input.mat="bmat",
+  genes=genes.gr,
+  do.par=TRUE,
+  num.cores=10
+);
+
+# smooth the cell-by-gene matrix
+message(sprintf("Smoothing the cell-by-gene matrix\n"))
+x.sp = runMagic(
+  obj=x.sp,
+  input.mat="gmat",
+  step.size=3
+);
+
+message(sprintf("Writing the cell by gene data to a Matrix Market format file\n"))
+cellByGeneData <- x.sp@gmat;
+cellByGeneMatrix <- as.matrix(cellByGeneData)
+cellByGene <- as.data.frame(cellByGeneMatrix);
+
+# Write cell by gene sparse matrix in Matrix Market format not CSV format
+#write.csv(cellByGene, file = "cellByGeneData.csv");
+writeMM(obj = cellByGeneData, file = "cellByGene.mtx")
+
+
+# We don't need the cell by gene summary csv except for perhaps debugging
+#cellByGeneSummary <- summary(cellByGeneData);
 #
-## Identify peaks using selected cells. Fragments belonging to a subset of cells
-## are extracted and used to identify peaks using MACS2. This function requires
-## "MACS2" and "snaptools" preinstalled and excutable.
-## https://rdrr.io/github/r3fang/SnapATAC/man/runMACS.html
-#runMACS(
-#  obj=x.sp[which(x.sp@cluster==1),],
-#  output.prefix="Peaks.1",
-#  path.to.snaptools="/usr/local/bin/snaptools",
-#  path.to.macs="/usr/local/bin/macs2",
-#  #gsize: effective genome size. 'hs' for human, 'mm' for mouse, 'ce' for C. elegans, 'dm' for fruitfly (default: None)
-#  gsize="hs",
-#  buffer.size=500,
-#  num.cores=5,
-#  macs.options="--nomodel --shift 37 --ext 73 --qval 1e-2 -B --SPMR --call-summits",
-#  tmp.folder=tempdir()
-#  );
+## Rename the summary column names i,j
+##https://stackoverflow.com/questions/7531868/how-to-rename-a-single-column-in-a-data-frame
+#names(cellByGeneSummary)[names(cellByGeneSummary) == 'i'] <- 'BarcodeID'
+#names(cellByGeneSummary)[names(cellByGeneSummary) == 'j'] <- 'GeneID'
 #
-## Next, we provide a short script that performs this step for all clusters.
+## Update: only including the numeric barcode IDs in most files, since most people won't
+## care about the actual barcodes for most uses, and including these in every single
+## file just has the effect of inflating the file sizes. If people actually do care about
+## the barcode sequence of a cell, they'll still be able to use 'cellBarcodes.csv' to obtain it.
+### Add barcodes column to cell by bin 
+##cellByGeneSummary$Barcode <- BarcodeLookupTable[match(cellByGeneSummary$BarcodeID, BarcodeLookupTable$BarcodeID), "Barcode"]
 #
-## call peaks for all cluster with more than 100 cells
-#clusters.sel = names(table(x.sp@cluster))[which(table(x.sp@cluster) > 200)];
-#peaks.ls = mclapply(seq(clusters.sel), function(i){
-#  print(clusters.sel[i]);
-#  runMACS(
-#      obj=x.sp[which(x.sp@cluster==clusters.sel[i]),], 
-#      output.prefix=paste0("Peaks_", gsub(" ", "_", clusters.sel)[i]),
-#      path.to.snaptools="/usr/local/bin/snaptools",
-#      path.to.macs="/usr/local/bin/macs2",
-#      gsize="hs", # mm, hs, etc
-#      buffer.size=500, 
-#      num.cores=1,
-#      macs.options="--nomodel --shift 100 --ext 200 --qval 5e-2 -B --SPMR",
-#      tmp.folder=tempdir()
-# );
-#}, mc.cores=5);
+## Get the gene names from the sparse matrix
+#summaryGeneNames <- colnames(cellByGene)[cellByGeneSummary$GeneID]
+#cellByGeneSummary$Gene <- summaryGeneNames
 #
-##assuming all .narrowPeak files in the current folder are generated from the clusters
-#peaks.names = system("ls | grep narrowPeak", intern=TRUE);
-#peak.gr.ls = lapply(peaks.names, function(x){
-#  peak.df = read.table(x)
-#  GRanges(peak.df[,1], IRanges(peak.df[,2], peak.df[,3]))
-#})
-#peak.gr = reduce(Reduce(c, peak.gr.ls));
-#peak.gr
-#
-#message(sprintf("Creating a cell by peak matrix\n"))
-## Step 12. Create a cell-by-peak matrix
-## Using merged peak list as a reference, we next create a cell-by-peak matrix using the original snap file.
-#
-#peaks.df = as.data.frame(peak.gr)[,1:3];
-#
-#write.csv(peaks.df, file = "peaksAllCells.csv", row.names = FALSE);
-#
+#message(sprintf("Writing the cell by gene summary to a csv\n"))
+#write.csv(cellByGeneSummary, file = "cellByGene_summary.csv", row.names = FALSE);
+
+message(sprintf("Writing the gene id to gene name lookup table to a csv\n"))
+# Create a lookup table for gene ID to gene name string
+#http://best-answer.net/easy-way-to-perform-a-lookup-in-r/
+geneNames <- colnames(cellByGene)
+geneIDs <- c(1:length(geneNames))
+geneLookupTable <- data.frame("GeneID" = geneIDs, "Gene" = geneNames);
+write.csv(geneLookupTable, file = "cellGenes.csv", row.names = FALSE);
+
+
+# Step 11. Identify peaks
+# Next we aggregate cells from the each cluster to create an ensemble track for
+# peak calling and visualization. This step will generate a .narrowPeak file that
+# contains the identified peak and .bedGraph file for visualization. To obtain
+# the most robust result, we don't recommend to perform this step for clusters
+# with cell number less than 100. In the below example, SnapATAC creates
+# atac_v1_adult_brain_fresh_5k.1_peaks.narrowPeak and 
+# atac_v1_adult_brain_fresh_5k.1_treat_pileup.bdg. bdg file can be compressed
+# to bigWig file using bedGraphToBigWig for IGV or Genome Browser visulization.
+message(sprintf("Identifying peaks\n"))
+
+# Identify peaks using selected cells. Fragments belonging to a subset of cells
+# are extracted and used to identify peaks using MACS2. This function requires
+# "MACS2" and "snaptools" preinstalled and excutable.
+# https://rdrr.io/github/r3fang/SnapATAC/man/runMACS.html
+runMACS(
+  obj=x.sp[which(x.sp@cluster==1),],
+  output.prefix="Peaks.1",
+  path.to.snaptools="/usr/local/bin/snaptools",
+  path.to.macs="/usr/local/bin/macs2",
+  #gsize: effective genome size. 'hs' for human, 'mm' for mouse, 'ce' for C. elegans, 'dm' for fruitfly (default: None)
+  gsize="hs",
+  buffer.size=500,
+  num.cores=5,
+  macs.options="--nomodel --shift 37 --ext 73 --qval 1e-2 -B --SPMR --call-summits",
+  tmp.folder=tempdir()
+  );
+
+# Next, we provide a short script that performs this step for all clusters.
+
+# call peaks for all cluster with more than 100 cells
+clusters.sel = names(table(x.sp@cluster))[which(table(x.sp@cluster) > 200)];
+peaks.ls = mclapply(seq(clusters.sel), function(i){
+  print(clusters.sel[i]);
+  runMACS(
+      obj=x.sp[which(x.sp@cluster==clusters.sel[i]),], 
+      output.prefix=paste0("Peaks_", gsub(" ", "_", clusters.sel)[i]),
+      path.to.snaptools="/usr/local/bin/snaptools",
+      path.to.macs="/usr/local/bin/macs2",
+      gsize="hs", # mm, hs, etc
+      buffer.size=500, 
+      num.cores=1,
+      macs.options="--nomodel --shift 100 --ext 200 --qval 5e-2 -B --SPMR",
+      tmp.folder=tempdir()
+ );
+}, mc.cores=5);
+
+#assuming all .narrowPeak files in the current folder are generated from the clusters
+peaks.names = system("ls | grep narrowPeak", intern=TRUE);
+peak.gr.ls = lapply(peaks.names, function(x){
+  peak.df = read.table(x)
+  GRanges(peak.df[,1], IRanges(peak.df[,2], peak.df[,3]))
+})
+peak.gr = reduce(Reduce(c, peak.gr.ls));
+peak.gr
+
+message(sprintf("Creating a cell by peak matrix\n"))
+# Step 12. Create a cell-by-peak matrix
+# Using merged peak list as a reference, we next create a cell-by-peak matrix using the original snap file.
+
+peaks.df = as.data.frame(peak.gr)[,1:3];
+
+write.csv(peaks.df, file = "peaksAllCells.csv", row.names = FALSE);
+
 
 
