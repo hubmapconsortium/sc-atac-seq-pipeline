153898cd0ab022f098a5e93a1bd4fe7aa40a7ebe
Matt Ruffalo <matt.ruffalo@gmail.com>
2020-12-01 14:08:40 -0500

    Fix now-enforced formatting

--- bin/concat_fastq.py
+++ bin/concat_fastq.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
+import json
 from argparse import ArgumentParser
+from os import fspath
 from pathlib import Path
 from pprint import pprint
-from os import fspath
 from subprocess import run
 from typing import Iterable, Tuple
-import json
 
 FASTQ_EXTENSIONS = [
-    'fastq',
-    'fastq.gz',
+    "fastq",
+    "fastq.gz",
 ]
 
 # No point in involving the Python interpreter in this decompression;
 # it'll be a lot faster just to pipe 'gunzip -c' to each file
 GUNZIP_COMMAND_TEMPLATE = [
-    'gunzip',
-    '-c',
-    '{fastq_file}',
+    "gunzip",
+    "-c",
+    "{fastq_file}",
 ]
 
-FOUND_PAIR_COLOR = '\033[01;32m'
-UNPAIRED_COLOR = '\033[01;31m'
-NO_COLOR = '\033[00m'
+FOUND_PAIR_COLOR = "\033[01;32m"
+UNPAIRED_COLOR = "\033[01;31m"
+NO_COLOR = "\033[00m"
 
-MERGED_FASTQ_R1 = Path('merged_R1.fastq')
-MERGED_FASTQ_R2 = Path('merged_R2.fastq')
-MERGED_FASTQ_BARCODE = Path('merged_barcode.fastq')
+MERGED_FASTQ_R1 = Path("merged_R1.fastq")
+MERGED_FASTQ_R2 = Path("merged_R2.fastq")
+MERGED_FASTQ_BARCODE = Path("merged_barcode.fastq")
+
 
 def find_r1_fastq_files(directory: Path) -> Iterable[Path]:
-    pattern = '**/*_R1*.{extension}'
+    pattern = "**/*_R1*.{extension}"
     for extension in FASTQ_EXTENSIONS:
         yield from directory.glob(pattern.format(extension=extension))
+
 
 def find_fastq_files(directory: Path) -> Iterable[Tuple[Path, Path, Path]]:
     """
@@ -42,29 +44,28 @@ def find_fastq_files(directory: Path) -> Iterable[Tuple[Path, Path, Path]]:
      [2] R3 FASTQ file
     """
     for r1_fastq_file in find_r1_fastq_files(directory):
-        r2_fastq_filename = r1_fastq_file.name.replace('_R1', '_R2')
-        r3_fastq_filename = r1_fastq_file.name.replace('_R1', '_R3')
+        r2_fastq_filename = r1_fastq_file.name.replace("_R1", "_R2")
+        r3_fastq_filename = r1_fastq_file.name.replace("_R1", "_R3")
         r2_fastq_file = r1_fastq_file.with_name(r2_fastq_filename)
         r3_fastq_file = r1_fastq_file.with_name(r3_fastq_filename)
 
         if r2_fastq_file.is_file() and r3_fastq_file.is_file():
-            print(FOUND_PAIR_COLOR + 'Found three FASTQ files:' + NO_COLOR)
-            print(f'\t{r1_fastq_file}')
-            print(f'\t{r2_fastq_file}')
-            print(f'\t{r3_fastq_file}')
+            print(FOUND_PAIR_COLOR + "Found three FASTQ files:" + NO_COLOR)
+            print(f"\t{r1_fastq_file}")
+            print(f"\t{r2_fastq_file}")
+            print(f"\t{r3_fastq_file}")
             yield r1_fastq_file, r2_fastq_file, r3_fastq_file
         else:
-            print(UNPAIRED_COLOR + 'Found lone FASTQ file:' + NO_COLOR)
-            print('\t', r1_fastq_file, sep='')
+            print(UNPAIRED_COLOR + "Found lone FASTQ file:" + NO_COLOR)
+            print("\t", r1_fastq_file, sep="")
+
 
 def decompress_concat_fastq(input_fastq: Path, merged_fastq: Path):
-    print('Concatenating', input_fastq, 'to', merged_fastq)
-    with open(merged_fastq, 'ab') as out:
-        command = [
-            piece.format(fastq_file=input_fastq)
-            for piece in GUNZIP_COMMAND_TEMPLATE
-        ]
+    print("Concatenating", input_fastq, "to", merged_fastq)
+    with open(merged_fastq, "ab") as out:
+        command = [piece.format(fastq_file=input_fastq) for piece in GUNZIP_COMMAND_TEMPLATE]
         run(command, stdout=out, check=True)
+
 
 def main(directories: Iterable[Path]):
     for directory in directories:
@@ -77,9 +78,10 @@ def main(directories: Iterable[Path]):
             decompress_concat_fastq(r2_fastq, MERGED_FASTQ_BARCODE)
             decompress_concat_fastq(r3_fastq, MERGED_FASTQ_R2)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     p = ArgumentParser()
-    p.add_argument('directory', type=Path, nargs='+')
+    p.add_argument("directory", type=Path, nargs="+")
     args = p.parse_args()
 
     main(args.directory)
