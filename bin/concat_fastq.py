53415520f94ac0d9a1fae89af0f6e8250240723a
Matt Ruffalo <matt.ruffalo@gmail.com>
2020-12-01 14:15:04 -0500

    Use fastq-utils package in bin/concat_fastq.py

--- bin/concat_fastq.py
+++ bin/concat_fastq.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
-import json
 from argparse import ArgumentParser
-from os import fspath
 from pathlib import Path
-from pprint import pprint
 from subprocess import run
-from typing import Iterable, Tuple
+from typing import Iterable
 
-FASTQ_EXTENSIONS = [
-    "fastq",
-    "fastq.gz",
-]
+from fastq_utils import find_grouped_fastq_files
 
 # No point in involving the Python interpreter in this decompression;
 # it'll be a lot faster just to pipe 'gunzip -c' to each file
@@ -19,45 +13,9 @@ GUNZIP_COMMAND_TEMPLATE = [
     "-c",
     "{fastq_file}",
 ]
-
-FOUND_PAIR_COLOR = "\033[01;32m"
-UNPAIRED_COLOR = "\033[01;31m"
-NO_COLOR = "\033[00m"
-
 MERGED_FASTQ_R1 = Path("merged_R1.fastq")
 MERGED_FASTQ_R2 = Path("merged_R2.fastq")
 MERGED_FASTQ_BARCODE = Path("merged_barcode.fastq")
-
-
-def find_r1_fastq_files(directory: Path) -> Iterable[Path]:
-    pattern = "**/*_R1*.{extension}"
-    for extension in FASTQ_EXTENSIONS:
-        yield from directory.glob(pattern.format(extension=extension))
-
-
-def find_fastq_files(directory: Path) -> Iterable[Tuple[Path, Path, Path]]:
-    """
-    :param directory:
-    :return: Iterable of 3-tuples:
-     [0] R1 FASTQ file
-     [1] R2 FASTQ file
-     [2] R3 FASTQ file
-    """
-    for r1_fastq_file in find_r1_fastq_files(directory):
-        r2_fastq_filename = r1_fastq_file.name.replace("_R1", "_R2")
-        r3_fastq_filename = r1_fastq_file.name.replace("_R1", "_R3")
-        r2_fastq_file = r1_fastq_file.with_name(r2_fastq_filename)
-        r3_fastq_file = r1_fastq_file.with_name(r3_fastq_filename)
-
-        if r2_fastq_file.is_file() and r3_fastq_file.is_file():
-            print(FOUND_PAIR_COLOR + "Found three FASTQ files:" + NO_COLOR)
-            print(f"\t{r1_fastq_file}")
-            print(f"\t{r2_fastq_file}")
-            print(f"\t{r3_fastq_file}")
-            yield r1_fastq_file, r2_fastq_file, r3_fastq_file
-        else:
-            print(UNPAIRED_COLOR + "Found lone FASTQ file:" + NO_COLOR)
-            print("\t", r1_fastq_file, sep="")
 
 
 def decompress_concat_fastq(input_fastq: Path, merged_fastq: Path):
@@ -69,7 +27,7 @@ def decompress_concat_fastq(input_fastq: Path, merged_fastq: Path):
 
 def main(directories: Iterable[Path]):
     for directory in directories:
-        for r1_fastq, r2_fastq, r3_fastq in find_fastq_files(directory):
+        for r1_fastq, r2_fastq, r3_fastq in find_grouped_fastq_files(directory, 3):
             # Flipping R2 and R3 here is deliberate. For the input data, barcodes
             # are in R2, and R1 and R3 are the two reads which should be aligned
             # to the genome. Internally in this pipeline, it's more straightforward
