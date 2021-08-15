7a74dc399fd47c27f947ff141db4e72b061adafd
Walt Shands <jshands@ucsc.edu>
2021-08-15 04:48:34 +0000

    changes to support Archr

--- install_R_packages.R
+++ install_R_packages.R
@@ -1,39 +1,90 @@
 install.packages(
-	c(
-		'BiocManager',
-		'Matrix',
-		'devtools',
-		'doSNOW',
-		'optparse',
-		'plot3D',
-		'umap'
-	),
-	Ncpus=6
+c(
+  'optparse'
+  ),
+ Ncpus=6
 )
-# Install SnapATAC
-library(devtools)
 
-install_github('r3fang/SnapATAC')
-# Install R packages using BioConductor
-# at https://bioconductor.org/install/#install-bioconductor-packages
+# ArchR is designed to be run on Unix-based operating systems such as macOS and linux.
+# ArchR is NOT supported on Windows or other operating systems.
 
-# rtracklayer needed for cell selection work around described at
-#      https://github.com/r3fang/SnapATAC/issues/139
-# RFLPtools needed for the write.hclust call
-# rmarkdown used when running an R markdown document
-# dplyr needed for R markdown to create PDF files
-BiocManager::install(
-    c(
-		'BSgenome.Hsapiens.NCBI.GRCh38',
-		'JASPAR2016',
-		'RFLPtools',
-		'SummarizedExperiment',
-		'chromVAR',
-		'dplyr',
-		'motifmatchr',
-		'rmarkdown',
-		'rtracklayer'
-	),
-	ask=FALSE,
-	Ncpus=6
+# ArchR installation currently requires devtools and BiocManager for installation
+# of GitHub and Bioconductor packages. Run the following commands to install the
+# various dependencies used by ArchR:
+
+# Docker image rocker/tidyverse includes devtools so no need to install it
+# First, install devtools (for installing GitHub packages) if it isn’t already installed:
+#if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
+
+# Then, install BiocManager (for installing bioconductor packages) if it isn’t already installed:
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+
+tryCatch({
+    BiocManager::install("DirichletMultinomial")
+},
+    error = function(e) {
+    message("Error installing DirichletMultinomial")
+    message(e$message)
+    quit("no", -1)
+  }
 )
+
+# Then, install ArchR:
+#devtools::install_github("GreenleafLab/ArchR", ref="release_1.0.2", repos = BiocManager::repositories())
+tryCatch({
+    devtools::install_github("GreenleafLab/ArchR", ref="release_1.0.2", repos = BiocManager::repositories())
+},
+    error = function(e) {
+    message("Error installing DirichletMultinomial")
+    message(e$message)
+    quit("no", -1)
+  }
+)
+
+## Lastly, install all of the ArchR dependencies that arent installed by default:
+library(ArchR)
+
+#ArchR::installExtraPackages()
+tryCatch({
+    ArchR::installExtraPackages()
+},
+    error = function(e) {
+    message("Error installing ArchR extra packages")
+    message(e$message)
+    quit("no", -1)
+  }
+)
+
+#
+## If any of these steps fails, you should identify the offending package and
+## troubleshoot that individual installation before proceeding. The one exception
+## is Cairo (see below) which is installed by the ArchR::installExtraPackages()
+## function. Cairo is not required but is highly recommended.
+
+# We installed MACS2 using python and archr_requirements.txt
+## It is also highly recommended that you install MACS2, which requires python,
+## and have the macs2 executable in your PATH variable. This will allow ArchR to call peaks using MACS2.
+##pip install MACS2
+#
+#devtools::install_github("GreenleafLab/chromVARmotifs")
+tryCatch({
+    devtools::install_github("GreenleafLab/chromVARmotifs")
+},
+    error = function(e) {
+    message("Error installing GreenleafLab chromVARmotifs")
+    message(e$message)
+    quit("no", -1)
+  }
+)
+
+#install.packages("magick")
+tryCatch({
+    install.packages("magick")
+},
+    error = function(e) {
+    message("Error installing magick")
+    message(e$message)
+    quit("no", -1)
+  }
+)
+
