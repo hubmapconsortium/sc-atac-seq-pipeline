46caeb2a0772c11b39121b39e8f071b19a4f92b5
Walt Shands <jshands@ucsc.edu>
2021-08-16 19:14:20 +0000

    remove prints statements and commented out code

--- bin/snaptools_wrapper.py
+++ bin/snaptools_wrapper.py
@@ -27,12 +27,6 @@ SNAPTOOLS_COMMAND_DEFAULTS: Dict[str, List[CommandData]] = {
     "mem": [
         ("--alignment-index", snaptools_defaults.DEFAULT_ALIGNMENT_INDEX, find_base_index_path),
     ]
-    #    "align-paired-end": [
-    #        ("--input-reference", snaptools_defaults.DEFAULT_ALIGNMENT_INDEX, find_base_index_path),
-    #    ],
-    #    "snap-pre": [
-    #        ("--genome-size", snaptools_defaults.DEFAULT_SIZE_INDEX, identity),
-    #    ],
 }
 
 
@@ -44,11 +38,8 @@ def run_with_defaults(snaptools_command: str, other_args: List[Union[Path, str]]
     # finding the "base" path of a genome index). If the option is not present,
     # append with the default value
     for option, default, func in SNAPTOOLS_COMMAND_DEFAULTS[snaptools_command]:
-        print("option {} default {} func {}".format(option, default, func))
-        print("args {}".format(args))
         if option in args:
             existing_option_index = args.index(option)
-            print("existing option index: {}".format(existing_option_index))
             value_index = existing_option_index + 1
             # args[value_index] = func(args[value_index])
             # insert the reference genome index path just after 'bwa mem'
@@ -56,17 +47,14 @@ def run_with_defaults(snaptools_command: str, other_args: List[Union[Path, str]]
             existing_option_value = args[value_index]
             args.remove(option)
             args.remove(existing_option_value)
-            print("args before insertion: {}".format(args))
             args.insert(0, func(existing_option_value))
         else:
             # insert the reference genome index path just after 'bwa mem'
             args.insert(0, (func(default)))
 
     command = ["bwa", snaptools_command]
-    # command = ["snaptools", snaptools_command]
     command.extend(fspath(arg) for arg in args)
 
-    print("Running:", " ".join(command))
     run(command, check=True)
 
 
@@ -79,8 +67,5 @@ if __name__ == "__main__":
         help="Arguments to the chosen snaptools command",
     )
     args = p.parse_args()
-    print("args {}".format(args))
-    print("snaptools command: {}".format(args.snaptools_command))
-    print("other args: {}".format(args.other_arg))
 
     run_with_defaults(args.snaptools_command, args.other_arg)
