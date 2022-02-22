175677c7f536d0414aeb19980c4dd39e33a0d1d5
Matt Ruffalo <matt.ruffalo@gmail.com>
2022-02-22 12:41:49 -0500

    Fix annotations

--- bin/write_genome_build.py
+++ bin/write_genome_build.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import json
 
-genome_build_data = {"genome": "grch38", "annotations": {"source": "GENCODE", "version": 35}}
+genome_build_data = {"genome": "hg38", "annotations": {"source": "ucsc", "version": 38}}
 
 if __name__ == "__main__":
     with open("genome_build.json", "w") as f:
